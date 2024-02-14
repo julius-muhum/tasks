8dd4b6c37fac6e033d16eea3bd1ae109612e73ec
Julius Muhumuza <juliusnm@udel.edu>
2024-02-13 21:59:52 -0500

    maybe

--- src/functions.ts
+++ src/functions.ts
@@ -30,7 +30,8 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return message.toUpperCase + "!";
+    const newWord: string = message.toUpperCase + "!";
+    return newWord;
 }
 
 /**
