fc946b5c77be896e33f319400c2accba36a82d79
Julius Muhumuza <juliusnm@udel.edu>
2024-02-28 15:11:30 -0500

    Fixed Conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
+//HEAD;
 import { Button, Col, Container, Row } from "react-bootstrap";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
+//upstream/task-state
 
 function App(): JSX.Element {
     return (
@@ -18,7 +17,6 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript. By Julius Muhumuza.
                 Hello World!
             </header>
-<<<<<<< HEAD
             <h2>Second header for Task 3!</h2>
             <img
                 src={require("./picture for task3.jpeg")}
@@ -75,7 +73,6 @@ function App(): JSX.Element {
                     </Col>
                 </Row>
             </Container>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -88,7 +85,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
